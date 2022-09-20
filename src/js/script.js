32c38c8e62bde2e25795633ecd89213b50f73b43
andrey <example174now@gmail.com>
2022-09-20 23:15:03 +0500

    three adapted sections

---
+++ src/js/script.js
@@ -0,0 +1,73 @@
+var name = "Andrey";
+let number = 7;
+const  pi = 3.14;
+
+number = 4;
+
+
+
+let leftBorderWdth = 200;
+
+//number
+//string  - "", '', ``
+//true/false 
+//null
+//undefined 
+//let obj = {
+//    name: 'apple',
+//    color: 'green',
+//    weight: 200
+//}
+//symbol
+
+// alert(1234)
+// console.log(number);
+// let answer = confirm("Вам есть 18");
+// console.log(answer);
+
+// let answer = prompt("Вам есть 18" , "");
+// console.log(answer);
+
+// console.log(4+4);
+
+// let isCheked = false,
+//     isClose = false;
+
+// console.log(isCheked && isClose);
+
+// console.log(isCheked || isClose);
+
+// if (2*6 ==  8*1) {
+//     console.log('Верно')
+// } else {
+//     console.log('Ошибка')
+// }
+
+// let answer = confirm("Вам есть 18");
+// if (answer) {
+//     console.log('Проходите')
+// } else {
+//     console.log('Уходи')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Неправильно')
+// } else if ( num > 100) {
+//     console.log('Много')
+// } else {
+//     console.log('Верно')
+// }
+
+// for(let i = 1; i < 8; i++) {
+//     console.log(i)
+// }
+
+function logging(a, b) {
+    console.log(a + b)
+}
+
+logging(3,5);
+
+logging(6,8);
