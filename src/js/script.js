09e4a82679e1f62f3bd11c60085e3f2b65fc831a
andrey <example174now@gmail.com>
2022-10-25 00:37:21 +0500

    made and adapted

--- src/js/script.js
+++ src/js/script.js
@@ -1,73 +1,40 @@
-var name = "Andrey";
-let number = 7;
-const  pi = 3.14;
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        dotsClass: 'slick-dots',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
 
-number = 4;
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
 
 
 
-let leftBorderWdth = 200;
+     function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                 e.preventDefault();
+                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                 $('.catalog-item__list ').eq(i).toggleClass('catalog-item__list_active');
+            }) 
+         });
+     };
 
-//number
-//string  - "", '', ``
-//true/false 
-//null
-//undefined 
-//let obj = {
-//    name: 'apple',
-//    color: 'green',
-//    weight: 200
-//}
-//symbol
-
-// alert(1234)
-// console.log(number);
-// let answer = confirm("Вам есть 18");
-// console.log(answer);
-
-// let answer = prompt("Вам есть 18" , "");
-// console.log(answer);
-
-// console.log(4+4);
-
-// let isCheked = false,
-//     isClose = false;
-
-// console.log(isCheked && isClose);
-
-// console.log(isCheked || isClose);
-
-// if (2*6 ==  8*1) {
-//     console.log('Верно')
-// } else {
-//     console.log('Ошибка')
-// }
-
-// let answer = confirm("Вам есть 18");
-// if (answer) {
-//     console.log('Проходите')
-// } else {
-//     console.log('Уходи')
-// }
-
-// const num = 50;
-
-// if (num < 49) {
-//     console.log('Неправильно')
-// } else if ( num > 100) {
-//     console.log('Много')
-// } else {
-//     console.log('Верно')
-// }
-
-// for(let i = 1; i < 8; i++) {
-//     console.log(i)
-// }
-
-function logging(a, b) {
-    console.log(a + b)
-}
-
-logging(3,5);
-
-logging(6,8);
+     toggleSlide('.catalog-item__link');
+     toggleSlide('.catalog-item__back');
+  });
